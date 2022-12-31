8c24628ea62cf22cc8a1af8028b047477a699d95
LongNguyen_DDC <25nguyenduclong@gmail.com>
2022-12-31 21:33:50 +0700

    tạo them login

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {GoogleAuthProvider,signInWithPopup} from 'firebase/auth';
+import { auth } from '../firebase/firebase';
 
 export const Navigation = () => {
+
+
+  const singIn =  () => {
+  const singinGoogle = new GoogleAuthProvider();
+       signInWithPopup(auth,singinGoogle);
+  }
+
+  const handleClick = async () => {
+     await singIn();
+  };
   return (
-    <header>
-        <div>logo</div>
-    </header>
-  )
+      <header>
+          <div>logo</div>
+          <div className="right-action">
+              <button onClick={(e) => handleClick()}>login</button>
+          </div>
+      </header>
+  );
 }
